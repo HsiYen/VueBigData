db399f06a0f43fc7d730b6c4c08380921c9daf8e
Jinx <649728226@qq.com>
2019-11-21 17:18:04 +0800

    大数据未完

---
+++ BigData/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <router-view />
+  </div>
+</template>
+
+<script>
+$(function () {
+  alert("1324");
+})
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  border: 1px solid black;
+}
+</style>
---
+++ BigData/src/views/page/page.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="BackImg"></div>
+    <div>这里是入口</div>
+  </div>
+</template>
+<style>
+.BackImg {
+  width: 100vw;
+  height: 100vh;
+  background-color: black;
+}
+</style>
+<script>
+export default {
+  name: "page"//入口
+}
+</script>
